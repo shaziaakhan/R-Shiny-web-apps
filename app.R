6547b0c4f7e85b8c8b92d42c406f45de5ed94890
shaziaakhan <shazia.takhan@gmail.com>
2023-12-22 21:57:35 +0530

    changes

--- app.R
+++ app.R
@@ -27,7 +27,7 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                 ) 
 ) 
 
-# Define server function  
+# Defining server function  
 server <- function(input, output) {
   output$txtout <- renderText({
     paste( input$txt1, input$txt2, sep = " " )
