b609c0c511dd75719f1a11b2b62ab5415874590e
shaziaakhan <shazia.takhan@gmail.com>
2023-12-24 11:54:47 +0530

    webpage-slider input

---
+++ Iris Predictor/app-slider.R
@@ -0,0 +1,93 @@
+
+library(shiny)
+library(data.table)
+library(randomForest)
+
+# Read in the RF model
+model <- readRDS("C:/Users/shazi/OneDrive/Documents/model.rds")
+
+# Training set
+TrainSet <- read.csv("C:/Users/shazi/OneDrive/Documents/training.csv", header = TRUE)
+TrainSet <- TrainSet[,-1]
+
+
+ui <- pageWithSidebar(
+  
+  # Page header
+  headerPanel('Iris Predictor'),
+  
+  # Input values
+  sidebarPanel(
+    HTML("<h3>Input parameters</h4>"),
+    sliderInput("Sepal.Length", label = "Sepal Length", value = 5.0,
+                min = min(TrainSet$Sepal.Length),
+                max = max(TrainSet$Sepal.Length)
+    ),
+    sliderInput("Sepal.Width", label = "Sepal Width", value = 3.6,
+                min = min(TrainSet$Sepal.Width),
+                max = max(TrainSet$Sepal.Width)),
+    sliderInput("Petal.Length", label = "Petal Length", value = 1.4,
+                min = min(TrainSet$Petal.Length),
+                max = max(TrainSet$Petal.Length)),
+    sliderInput("Petal.Width", label = "Petal Width", value = 0.2,
+                min = min(TrainSet$Petal.Width),
+                max = max(TrainSet$Petal.Width)),
+    
+    actionButton("submitbutton", "Submit", class = "btn btn-primary")
+  ),
+  
+  mainPanel(
+    tags$label(h3('Status/Output')), # Status/Output Text Box
+    verbatimTextOutput('contents'),
+    tableOutput('tabledata') # Prediction results table
+    
+  )
+)
+
+server<- function(input, output, session) {
+  
+  # Input Data
+  datasetInput <- reactive({  
+    
+    df <- data.frame(
+      Name = c("Sepal Length",
+               "Sepal Width",
+               "Petal Length",
+               "Petal Width"),
+      Value = as.character(c(input$Sepal.Length,
+                             input$Sepal.Width,
+                             input$Petal.Length,
+                             input$Petal.Width)),
+      stringsAsFactors = FALSE)
+    
+    Species <- 0
+    df <- rbind(df, Species)
+    input <- transpose(df)
+    write.table(input,"input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
+    
+    test <- read.csv(paste("input", ".csv", sep=""), header = TRUE)
+    
+    Output <- data.frame(Prediction=predict(model,test), round(predict(model,test,type="prob"), 3))
+    print(Output)
+    
+  })
+  
+  # Status/Output Text Box
+  output$contents <- renderPrint({
+    if (input$submitbutton>0) { 
+      isolate("Calculation complete.") 
+    } else {
+      return("Server is ready for calculation.")
+    }
+  })
+  
+  # Prediction results table
+  output$tabledata <- renderTable({
+    if (input$submitbutton>0) { 
+      isolate(datasetInput()) 
+    } 
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
